4e84c2fb37b9e9711550c61e24c2f0059fde97aa
Сергей <linnik_sergey@mail.ru>
2025-05-20 12:52:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(100)  # Начальный баланс 100
+    account = BankAccount(100)
 
-    print(account)  # Вывод начального баланса
-
+    print(account)
     account.deposit(50)
     account.withdraw(30)
     account.withdraw(200)  # Попытка снять больше, чем есть
+    account.withdraw(-10)  # Некорректная сумма
+    account.deposit(-5)    # Некорректная сумма
 
     print(f"Финальный баланс: {account.get_balance()}")
 
